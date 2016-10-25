024aaf1b3fe5470ffc28e2a9d2c57936038d6132
Dave Aditya <adave@adobe.com>
2016-10-25 12:48:15 -0600

    none

--- functions.php
+++ functions.php
@@ -11,12 +11,15 @@ function install_to_complain($conn)
   $conn = new mysqli($servername, $username, $password, $dbname);
   if ($conn->connect_error)
   {
-      die("Connection failed: " . $conn->connect_error);
+      echo "Connection failed: " . $conn->connect_error;
   }
 
   //
-  $sql = "SELECT SalesRep, Technician, SaleDate, DateOpened FROM AmpService LIMIT 1";
+  $sql = "SELECT SalesRep, Technician, CustomerName, SaleDate, DateOpened FROM AmpService LIMIT 1";
   $result = $conn->query($sql);
+  $numResults = mysql_num_rows($result);
+  $counter = 0
+
   if ($result->num_rows > 0)
   {
     while($row = $result->fetch_assoc())
@@ -25,17 +28,27 @@ function install_to_complain($conn)
         $technician = $row['Technician'];
         $sale_date = $row['SaleDate'];
         $date_opened = $row['DateOpened'];
+        $customer_name = $row['CustomerName'];
 
-        echo "$sales_rep $technician $sale_date $date_opened ";
+        echo "$sales_rep $technician $customer_name $sale_date $date_opened \n ";
 
         // modify output
         $temp_sd = explode(" ", $sale_date);
         $temp_do = explode(" ", $date_opened);
-        $temp_sd_2 = explode("/", $temp_sd[0]);
-        $temp_do_2 = explode("/", $temp_do[0]);
+        $sd = explode("/", $temp_sd[0]);
+        $do = explode("/", $temp_do[0]);
 
-        var_dump($temp_sd_2);
-        var_dump($temp_do_2);
+
+        if (++$counter == $numResults)
+        {
+          // last row
+          echo "[ '$customer_name' , new Date($sd[2], $sd[0], $sd[1]) , new Date($do[2], $do[0], $do[1]) ]";
+        }
+        else
+        {
+          // not last row
+          echo "[ '$customer_name' , new Date($sd[2], $sd[0], $sd[1]) , new Date($do[2], $do[0], $do[1]) ], ";
+        }
 
     }
   }
