e170d5eddfd0637f3b30f1ece8b9e1f6b3f44778
Dave Aditya <adave@adobe.com>
2016-10-25 12:12:04 -0600

    none

--- functions.php
+++ functions.php
@@ -7,20 +7,44 @@ $dbname = "test";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error)
+{
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM AmpService LIMIT 1";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        var_dump($row);
+
+
+function install_to_complain($conn)
+{
+
+  $sql = "SELECT SalesRep, Technician, SaleDate, DateOpened FROM AmpService LIMIT 1";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0)
+  {
+    while($row = $result->fetch_assoc())
+    {
+        $sales_rep = $row['SalesRep'];
+        $technician = $row['Technician'];
+        $sale_date = $row['SaleDate'];
+        $date_opened = $row['DateOpened'];
+
+        echo "$sales_rep $technician $sale_date $date_opened "; 
     }
-} else {
-    echo "0 results";
+  }
+  else
+  {
+      echo "0 results";
+  }
+
 }
+
+
+
+
+
+
+
+
+
 $conn->close();
-?>
