c88354e6a99638732790339ea74f5f4278f718be
Dave Aditya <adave@adobe.com>
2016-10-30 16:45:23 -0600

    none

---
+++ new_cost3.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['New Equipment Cost', 'Total Amount'],
+          ['Doorbell Cam - Silver',     1950],
+          ['Keyfob',   330   ],
+          ['Camera IP',  985],
+          ['Motion Sensors', 440],
+          ['Other',    285]
+        ]);
+
+        var options = {
+          title: 'New Equitment Cost (Service)',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="donutchart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
