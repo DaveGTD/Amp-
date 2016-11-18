c2e42db3059cf135633df7545075bc32507d90ab
Dave Aditya <adave@adobe.com>
2016-11-18 13:59:25 -0700

    none

--- calendar.php
+++ calendar.php
@@ -10,17 +10,17 @@
        dataTable.addColumn({ type: 'date', id: 'Date' });
        dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
        dataTable.addRows([
-          [ new Date(2012, 3, 13), 37032 ],
+          [ new Date(2012, 3, 13), 7032 ],
           [ new Date(2012, 3, 14), 38024 ],
           [ new Date(2012, 3, 15), 38024 ],
           [ new Date(2012, 3, 16), 38108 ],
           [ new Date(2012, 3, 17), 38229 ],
           // Many rows omitted for brevity.
           [ new Date(2013, 9, 4), 38177 ],
-          [ new Date(2013, 9, 5), 38705 ],
+          [ new Date(2013, 9, 5), 48705 ],
           [ new Date(2013, 9, 12), 38210 ],
           [ new Date(2013, 9, 13), 38029 ],
-          [ new Date(2013, 9, 19), 38823 ],
+          [ new Date(2013, 9, 19), 18823 ],
           [ new Date(2013, 9, 23), 38345 ],
           [ new Date(2013, 9, 24), 38436 ],
           [ new Date(2013, 9, 30), 38447 ]
